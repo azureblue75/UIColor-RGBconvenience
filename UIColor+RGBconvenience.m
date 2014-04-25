4ecb22549ed4641c48d3f0b78b4aebc834287cd5
Chris <chris@azureblue.org>
2014-04-25 11:16:33 +0100

    Correct divisor for achieving full range

--- UIColor+RGBconvenience.m
+++ UIColor+RGBconvenience.m
@@ -13,23 +13,23 @@
 + (UIColor*)colorWithRGB:(unsigned)rgb
 {
     float r,g,b;
-    
-    r = ((rgb >> 16) & 0xff) / 256.0;
-    g = ((rgb >>  8) & 0xff) / 256.0;
-    b = ((rgb >>  0) & 0xff) / 256.0;
-    
+
+    r = ((rgb >> 16) & 0xff) / 255.0;
+    g = ((rgb >>  8) & 0xff) / 255.0;
+    b = ((rgb >>  0) & 0xff) / 255.0;
+
     return [UIColor colorWithRed:r green:g blue:b alpha:1.0];
 }
 
 + (UIColor*)colorWithRGBA:(unsigned)rgba
 {
     float r,g,b,a;
-    
-    r = ((rgba >> 24) & 0xff) / 256.0;
-    g = ((rgba >> 16) & 0xff) / 256.0;
-    b = ((rgba >>  8) & 0xff) / 256.0;
-    a = ((rgba >>  0) & 0xff) / 256.0;
-    
+
+    r = ((rgba >> 24) & 0xff) / 255.0;
+    g = ((rgba >> 16) & 0xff) / 255.0;
+    b = ((rgba >>  8) & 0xff) / 255.0;
+    a = ((rgba >>  0) & 0xff) / 255.0;
+
     return [UIColor colorWithRed:r green:g blue:b alpha:a];
 }
 
