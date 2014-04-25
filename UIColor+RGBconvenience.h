ab2c0e895f522c71cd988257734a508b6247fc5c
Chris <chris@azureblue.org>
2014-04-25 11:08:42 +0100

    Initial commit
    A category on UIColor to allow colours to be specified by hex value
    (e.g. 0x3c4580)

---
+++ UIColor+RGBconvenience.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+RGBconvenience.h
+//  GameScorer
+//
+//  Created by Chris on 22/04/2014.
+//  Copyright (c) 2014 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (RGBconvenience)
+
++ (UIColor*)colorWithRGB:(unsigned)rgb;
++ (UIColor*)colorWithRGBA:(unsigned)rgba;
+
+@end
---
+++ UIColor+RGBconvenience.m
@@ -0,0 +1,36 @@
+//
+//  UIColor+RGBconvenience.m
+//  GameScorer
+//
+//  Created by Chris on 22/04/2014.
+//  Copyright (c) 2014 Chris. All rights reserved.
+//
+
+#import "UIColor+RGBconvenience.h"
+
+@implementation UIColor (RGBconvenience)
+
++ (UIColor*)colorWithRGB:(unsigned)rgb
+{
+    float r,g,b;
+    
+    r = ((rgb >> 16) & 0xff) / 256.0;
+    g = ((rgb >>  8) & 0xff) / 256.0;
+    b = ((rgb >>  0) & 0xff) / 256.0;
+    
+    return [UIColor colorWithRed:r green:g blue:b alpha:1.0];
+}
+
++ (UIColor*)colorWithRGBA:(unsigned)rgba
+{
+    float r,g,b,a;
+    
+    r = ((rgba >> 24) & 0xff) / 256.0;
+    g = ((rgba >> 16) & 0xff) / 256.0;
+    b = ((rgba >>  8) & 0xff) / 256.0;
+    a = ((rgba >>  0) & 0xff) / 256.0;
+    
+    return [UIColor colorWithRed:r green:g blue:b alpha:a];
+}
+
+@end
